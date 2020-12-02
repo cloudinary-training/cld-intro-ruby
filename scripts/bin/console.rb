e445bcfe7ca478cb9eff49c05e821825eabc5e95
Ariel Caplan <ariel.caplan@cloudinary.com>
2020-12-02 20:53:51 +0200

    Update console.rb to use plain IRB and config

--- scripts/bin/console.rb
+++ scripts/bin/console.rb
@@ -1,8 +1,6 @@
-# contents of bin/console.rb
-# https://cloudinary.com/console/cloudinary.yml
-# puts Cloudinary.config.cloud_name
-# Cloudinary::Utils.cloudinary_url('pizza', transformation: { width: 300, height: 500, crop: "crop", effect: "improve" })
-
 require 'cloudinary'
-ENV["CLOUDINARY_ENV"] = "development"
-binding.irb
+require __dir__ + '/../config.rb'
+puts Cloudinary.config.cloud_name
+
+require 'irb'
+IRB.start
